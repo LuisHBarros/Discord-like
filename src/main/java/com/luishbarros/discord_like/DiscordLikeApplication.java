1576203c640d3f825d6a1d3f3aee7689cae50801
luishbarros <luishrbr@outlook.com>
2026-02-17 19:42:07 -0300

    append: Basic Java Setup

---
+++ src/main/java/com/luishbarros/discord_like/DiscordLikeApplication.java
@@ -0,0 +1,13 @@
+package com.luishbarros.discord_like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordLikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordLikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luishbarros/discord_like/DiscordLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luishbarros.discord_like;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordLikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
